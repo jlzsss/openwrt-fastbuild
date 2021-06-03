fd24423922114794c1b124ce4554ce8a2bb002e8
jlzsss <jlz19821031@gmail.com>
2021-06-03 16:40:40 +0800

    Update custom.sh

--- user/x86_64/custom.sh
+++ user/x86_64/custom.sh
@@ -10,4 +10,8 @@ rm -rf ./package/feeds/packages/php7
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-ssr-libev-server/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+svn export --force https://github.com/project-lede/openwrt-app/branches/luci18/luci-app-autopoweroff package/luci-app-autopoweroff
+svn export --force https://github.com/project-lede/openwrt-app/branches/luci18/luci-app-autoreboot package/luci-app-autoreboot
+svn export --force https://github.com/project-lede/openwrt-app/branches/luci18/luci-app-autorepeater package/luci-app-autorepeater
+svn export --force https://github.com/project-lede/openwrt-app/branches/luci18/luci-app-chinadns-ng package//luci-app-chinadns-ng
 echo "=============================================="
